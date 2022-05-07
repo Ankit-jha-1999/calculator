d2f71d9fb43d3e8b7ec45460cb4100c0dfb03280
Ankit Jha <jhaankit3117@gmail.com>
2022-05-07 16:45:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React,{useState} from 'react';
+import './App.css';
+
+function App() {
+
+const [result, setResult]=useState("");
+
+const clickHandler=(event)=>{
+  setResult (result.concat(event.target.value))
+}
+
+const clearDisplay=()=>{
+  setResult ("")
+}
+
+const calculate=()=>{
+  setResult (eval(result).toString())
+}
+
+  return (
+    <div className="calc">
+      <input type= "text" placeholder='0' id="answer" value={result}/>
+      <input type="button" value="9" className='btn' onClick={clickHandler}/>
+      <input type="button" value="8" className='btn' onClick={clickHandler}/>
+      <input type="button" value="7" className='btn' onClick={clickHandler}/>
+      <input type="button" value="6" className='btn' onClick={clickHandler}/>
+      <input type="button" value="5" className='btn' onClick={clickHandler}/>
+      <input type="button" value="4" className='btn' onClick={clickHandler}/>
+      <input type="button" value="3" className='btn' onClick={clickHandler}/>
+      <input type="button" value="2" className='btn' onClick={clickHandler}/>
+      <input type="button" value="1" className='btn' onClick={clickHandler}/>
+      <input type="button" value="0" className='btn' onClick={clickHandler}/>
+      <input type="button" value="." className='btn' onClick={clickHandler}/>
+      <input type="button" value="+" className='btn' onClick={clickHandler}/>
+      <input type="button" value="-" className='btn' onClick={clickHandler}/>
+      <input type="button" value="*" className='btn' onClick={clickHandler}/>
+      <input type="button" value="/" className='btn' onClick={clickHandler}/>
+      <input type="button" value="%" className='btn' onClick={clickHandler}/>
+      <input type="button" value="Clear" className='btn btn1' onClick={clearDisplay}/>
+      <input type="button" value="=" className='btn btn1' onClick={calculate}/>
+    </div>
+  );
+}
+
+export default App;
